1b6472b4496ed618611f8bc4fbed3aafb9e10c9a
Fabio Lopes <fzlopes1@gmail.com>
2016-11-23 12:17:56 -0200

    Commit inicial do projeto

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Ecommerce de livros com Laravel 5.3
+
+## Passos para utilizar
+
+### composer update
+### Configurar o arquivo .env com as configurações do banco
+### php artisan migrate
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Authentication</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+
+  <body>
+
+    <div class="container">
+        @include('layouts.top-menu')
+
+        @yield('content')
+    </div> <!-- /container -->
+  </body>
+</html>
